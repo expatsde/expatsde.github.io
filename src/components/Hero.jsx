e1253addae7087dc2d73fb88c4376d1bbb913c17
Gokul Menon <gokul.menon@free-now.com>
2024-04-06 12:54:08 +0200

    added life in germany link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,7 +38,7 @@ const Hero = () => {
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
             We Facilitates Smooth Relocation to Germany, Ensuring a Seamless Transition for Individuals. 🛫 🏡
           </p>
-          <Button href="/pricing" white>
+          <Button href="https://mousy-relation-39a.notion.site/Life-In-Germany-b24781e2b4cd41ffb0f160879c2a3863?pvs=4" white>
             Life in Germany
           </Button>
         </div>
